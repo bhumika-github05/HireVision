6a0a7c1d74112ca3e9043e64f24c4d8ee98707ec
GourvitRana <gourvitrana724@gmail.com>
2026-05-11 23:19:22 +0530

    Initial HireVision setup with MySQL Connection

---
+++ src/main/java/com/hirevision/TestConnection.java
@@ -0,0 +1,19 @@
+package com.hirevision;
+
+import com.hirevision.util.DBConnection;
+
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection conn = DBConnection.getConnection();
+
+        if (conn != null) {
+            System.out.println("Database Connected Successfully!");
+        } else {
+            System.out.println("Connection Failed!");
+        }
+    }
+}
